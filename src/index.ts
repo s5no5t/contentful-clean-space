5db028bd47f2e25dd7d15b6193eb7430328eb354
jugglingthebits <stephan.oehlert@gmail.com>
2018-01-08 14:59:59 +0100

    Introduce tslint. Start building a contentful client.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
+// tslint:disable-next-line:no-reference
+///<reference path="./contentful-management.d.ts" />
+import { createManagementClient } from "contentful-management";
 import * as yargs from "yargs";
 
-const argv = yargs.option("space-id", {
-    type: "string",
-    describe: "Contentful space id",
-    demandOption: true
-}).option("access-token", {
-    type: "string",
-    describe: "Contentful access token",
-    demandOption: true
-}).version(false)
-    .parse();
+async function main() {
+    const argv = yargs.option("space-id", {
+        type: "string",
+        describe: "Contentful space id",
+        demandOption: true
+    }).option("access-token", {
+        type: "string",
+        describe: "Contentful access token",
+        demandOption: true
+    }).version(false)
+        .parse();
+
+    const contentfulManagementClient = createManagementClient({
+        accessToken: argv["access-token"]
+    });
+    const contentfulSpace = await contentfulManagementClient.getSpace(argv["space-id"]);
+}
+
+(async () => {
+    await main();
+})();
